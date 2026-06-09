59a871f8ac95e39dd2285c8ab54465fb6b3dc45c
susarroDEV <susarrodev@gmail.com>
2026-06-09 18:49:23 +0200

    Fixed lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,22 +4,17 @@ mod commands;
 mod business;
 mod infra;
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_store::Builder::default().build())
-        .invoke_handler(tauri::generate_handler![greet])
-        .invoke_handler(tauri::generate_handler![crate::commands::config::get_config])
-        .invoke_handler(tauri::generate_handler![crate::commands::auth::login])
-        .invoke_handler(tauri::generate_handler![crate::commands::auth::logout])
-        .invoke_handler(tauri::generate_handler![crate::commands::auth::current_session])
+        .invoke_handler(tauri::generate_handler![
+          crate::commands::config::get_config,
+          crate::commands::auth::login,
+          crate::commands::auth::logout,
+          crate::commands::auth::current_session
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
