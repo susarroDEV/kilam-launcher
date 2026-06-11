f70a91485ba797fe3bfad1a9161996f02cf685cf
susarroDEV <susarrodev@gmail.com>
2026-06-11 20:20:11 +0200

    Refactor EventList component to improve loading state handling and error display

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -4,39 +4,74 @@ import { EventDTO } from "../types/event_store";
 
 function EventList({uuid} : {uuid: string}) {
   const [events, setEvents] = useState<EventDTO[]>([])
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
 
+  
   useEffect(() => {
-    const load = async () => {
-      setEvents(await getActiveEvents(uuid))
+    let cancelled = false
+    
+    const load = async (uuid: string) => {
+      try {
+        setLoading(true)
+        const events = await getActiveEvents(uuid)
+        if (!cancelled) setEvents(events)
+      } catch (e) {
+        setError(String(e))
+      } finally {
+        setLoading(false)
+      }
     }
-    load()
+
+    load(uuid)
+    
+    return () => { 
+      cancelled = true 
+    }
   }, [uuid])
 
   return (
-    events.length == 0 ?
-    <p>
-      No hay eventos disponibles
-    </p>
-    :
-    <ul>
-      {
-        events.map(
-          dto => (
-            <li key={dto.event.id}>
-              <header>
-                {dto.status}
-              </header>
-              <h1>
-                {dto.event.name}
-              </h1>
+    <>
+      {(
+        loading ?
+        <p>
+          Cargando...
+        </p>
+        :
+        (
+          !error ?
+            (events.length == 0 ?
               <p>
-                {dto.event.description}
+                No hay eventos disponibles
               </p>
-            </li>
-          )
+              :
+              <ul>
+                {
+                events.map(
+                  dto => (
+                    <li key={dto.event.id}>
+                      <header>
+                        {dto.status}
+                      </header>
+                      <h1>
+                        {dto.event.name}
+                      </h1>
+                      <p>
+                        {dto.event.description}
+                      </p>
+                    </li>
+                  )
+                )
+              }
+              </ul>
+            )
+          :
+          <p>
+            Ha habido un error: {error}
+          </p>
         )
-      }
-    </ul>
+      )}
+    </>
   )
 }
 
