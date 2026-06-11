9125b85385f11ef2b50e442e5b526c69dd1d6e49
susarroDEV <susarrodev@gmail.com>
2026-06-11 20:06:43 +0200

    Refactor LoginScreen to handle form submission and loading state

--- src/pages/LoginScreen.tsx
+++ src/pages/LoginScreen.tsx
@@ -3,26 +3,51 @@ import { login } from "../lib/ipc"
 import { useAuth } from "../store/auth"
 
 function LoginScreen() {
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+
   const [username, setUsername] = useState('')
   const setProfile = useAuth((state) => state.setProfile)
 
-  async function handleLogin(username: string) {
-    const profile = await login(username)
-    setProfile(profile)
+  async function handleLogin(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    setLoading(true)
+    try {
+      const profile = await login(username)
+      setProfile(profile)
+    } catch (e) {
+      setError(String(e))
+    } finally {
+      setLoading(false)
+    }
   }
 
   return (
     <main>
-      <input
-        value={username}
-        onChange= {(e) => setUsername(e.target.value)}
-      >
-      </input>
-      <button
-        onClick={() => {handleLogin(username)}}
-      >
-        Login
-      </button>
+      <form onSubmit={handleLogin}>
+        <input
+          value={username}
+          onChange= {(e) => setUsername(e.target.value)}
+        >
+        </input>
+        <button
+          type="submit"
+          >
+          Login
+        </button>
+      </form>
+      {
+        loading &&
+        <p>
+          Cargando...
+        </p>
+      }
+      {
+        error != "" &&
+        <p>
+          Ha habido un error: {error}
+        </p>
+      }
     </main>
   )
 }
